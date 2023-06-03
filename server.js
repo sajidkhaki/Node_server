94fe1c9b2569bcfe259d824aae4bc96070cef019
Sajid.khaki <sajid@centurionco.com>
2023-06-03 17:24:55 +0530

    3commit

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app = express();
 app.get("/", function (req, res) {
   res.send("Simple Web Application is UP");
 });
+app.get("/test", function (req, res) {
+  res.send("Testing Page");
+});
 
 app.listen(8081, function () {
   console.log("Simple Web Application running on port 8081!");
